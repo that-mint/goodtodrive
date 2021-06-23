2ea57e1facc4fc49345a3718b145f2898ee587da
that_mint <47940073+that-mint@users.noreply.github.com>
2021-06-23 17:16:25 +1000

    added coinflip meme

--- bot/main.py
+++ bot/main.py
@@ -2,21 +2,33 @@ import discord
 from discord.ext import commands
 import os
 
+import random
+from random import choice
+
+determine_flip = [1, 0]
+
 client = commands.Bot(command_prefix=".")
 token = os.getenv("DISCORD_BOT_TOKEN")
 
 @client.event
 async def on_ready() :
-    await client.change_presence(activity = discord.Game("yeah nah good to drive boss"))
+    await client.change_presence(activity = discord.Game("GTDB | .help"))
     print("I am online")
 
 @client.command()
 async def ping(ctx) :
     await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
 
+@client.command(name="goodtodrive")
+async def goodtodrive(ctx) :
+    if random.choice(determine_flip) == 1:
+        await ctx.send(f"{ctx.message.author.mention} is good to drive! <:thepip:850738731274207262> 🏎️🌿")
+
+    else:
+        await ctx.send(f" {ctx.message.author.mention} isn't good to drive :( 🌿🚔🚨💥👪")
 @client.command(name="whoami")
 async def whoami(ctx) :
-    await ctx.send(f"You are {ctx.message.author.name}")
+    await ctx.send(f"You are {ctx.message.author.mention}")
 
 @client.command()
 async def clear(ctx, amount=3) :
