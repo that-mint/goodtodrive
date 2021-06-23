af969194a68e118c922a6ea91f29e9fda776ef42
that_mint <47940073+that-mint@users.noreply.github.com>
2021-06-23 20:59:41 +1000

    actual 10th try

--- bot/main.py
+++ bot/main.py
@@ -20,7 +20,7 @@ async def on_message(message):
         return
     elif "PIP" in message.content or "pip" in message.content:
         emoji = client.get_emoji(850738731274207262)
-        await message.add_reaction(message, emoji)
+        await message.add_reaction(emoji)
     else:
         await client.process_commands(message)
 
